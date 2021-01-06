9f14f8bd0727dce570341e7cc68b7d867a866c05
yds12 <28710965+yds12@users.noreply.github.com>
2021-01-06 12:54:09 +0100

    Drawing snake.

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ use ggez::event;
 use ggez::input::keyboard::{self, KeyCode};
 use rand::prelude::*;
 
-const SPEED: f32 = 100.0;
 const SCREEN: (f32, f32) = (800.0, 600.0);
 const TILES: (u16, u16) = (40, 30);
 const TILE_SIZE: (u16, u16) = (20, 20);
+const BG_COLOR: (u8, u8, u8) = (127, 127, 255);
+const SNAKE_COLOR: (u8, u8, u8) = (0, 255, 0);
+const FOOD_COLOR: (u8, u8, u8) = (0, 255, 0);
 
 enum Direction {
   Up,
@@ -26,8 +28,8 @@ struct GameState {
 impl GameState {
   pub fn new() -> Self {
     let mut rng = rand::thread_rng();
-    let mut snake = vec![];
-    let mut food = (0, 0);
+    let mut snake = vec![(1, 1), (2, 1), (3, 1)];
+    let mut food = (5, 5);
     let mut direction = Direction::Right;
 
     GameState {
@@ -35,6 +37,25 @@ impl GameState {
       food,
       direction
     }
+  }
+
+  fn draw_snake(&mut self, ctx: &mut Context) -> GameResult {
+    for cell in &self.snake {
+      let rect = graphics::Rect::new(
+        (cell.0 * TILE_SIZE.0) as f32,
+        (cell.1 * TILE_SIZE.1) as f32,
+        TILE_SIZE.0 as f32, TILE_SIZE.1 as f32);
+
+      let rect_mesh = graphics::Mesh::new_rectangle(ctx,
+        graphics::DrawMode::fill(), rect,
+        graphics::Color::from_rgb(
+          SNAKE_COLOR.0, SNAKE_COLOR.1, SNAKE_COLOR.2))?;
+
+        graphics::draw(ctx, &rect_mesh, graphics::DrawParam::default())?;
+
+    }
+
+    Ok(())
   }
 }
 
@@ -44,7 +65,11 @@ impl event::EventHandler for GameState {
   }
 
   fn draw(&mut self, ctx: &mut Context) -> GameResult {
-    graphics::clear(ctx, graphics::Color::from_rgb(127, 127, 255));
+    graphics::clear(ctx, graphics::Color::from_rgb(
+      BG_COLOR.0, BG_COLOR.1, BG_COLOR.2));
+
+    self.draw_snake(ctx);
+
     graphics::present(ctx)?;
     Ok(())
   }
@@ -52,7 +77,9 @@ impl event::EventHandler for GameState {
 
 fn main() -> GameResult {
   let cb = ggez::ContextBuilder::new("Snake", "Y.D.S.")
-    .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN.0, SCREEN.1));
+    .window_mode(ggez::conf::WindowMode::default()
+      .dimensions(SCREEN.0, SCREEN.1));
+
   let (ctx, event_loop) = &mut cb.build()?;
   graphics::set_window_title(ctx, "Rusty Snake: RUSNAKE");
 
